e058a4c4af47d6ae3ff386e9662b6621e6a2e156
Ashwin <ksashwinkumar3@gmail.com>
2024-03-20 15:39:50 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,13 @@
-var myUser=document.querySelector('h1#left');
+const myUser=document.querySelector('h1#left');
 function setUserName(){
     var user=prompt("Enter your name:");
     if (!user){
         setUserName();
-    };
-    // while (!user){
-    //     user=prompt("Enter a valid name:")
-    // } Another method in case the first doesn't work.
-    localStorage.setItem("name",user);
-    myUser.textContent=user;
+    }
+    else{
+        localStorage.setItem("name",user);
+        myUser.textContent=user;
+    };   
 };
 
 if (localStorage.getItem("name")){
